1ec5f79aca328e98cebe4769df898d4aac8779d7
muhammedbawery <95441115+muhammedbawery@users.noreply.github.com>
2022-12-23 17:59:40 +0100

    ...

---
+++ backend/project/main/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student 
+        fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate')
